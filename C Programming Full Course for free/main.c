9f9fa6e51914f016d5283ab1786e55cb2ee1c7fa
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-21 19:44:26 +0100

    a

--- C Programming Full Course for free/main.c
+++ C Programming Full Course for free/main.c
@@ -1,6 +1,10 @@
+// Preprocessor directiv
 #include <stdio.h>
 
 int main(void)
 {
-    
+    printf("Ilike pizza\n");
+    printf("It´s really good!");
+
+    return 0; // Program runs successfully, 1 if not (godd for compatibility for oder versions)
 }
---
+++ C Programming Full Course for free/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // variable = A reusable container for a value.
+    //            Behaves as if it were the value it contains
+
+    return 0;
+}

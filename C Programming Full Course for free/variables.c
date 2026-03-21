21fc122cf8d3a87c376008474aaef735258a360e
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-21 23:31:25 +0100

    a

--- C Programming Full Course for free/variables.c
+++ C Programming Full Course for free/variables.c
@@ -5,13 +5,19 @@ int main(void)
     // variable = A reusable container for a value.
     //            Behaves as if it were the value it contains
 
+    float gpa = 2.5; // great point average
+
+    printf("Your gpa is %f\n", gpa);
+
+/*
     int age = 25;
     int year = 2026;
-    int quantety = 1;
+    int quantety = 2;
 
     printf("You are %d years old\n", age); // %i works too (but its not a decimal)
     printf("The year is %d\n", year);
     printf("You have ordered %d x items\n", quantety);
+*/
 
     return 0;
 }

8d39dd75c82a8744eae6a69f376ac7c663549a49
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-21 21:03:45 +0100

    a

--- C Programming Full Course for free/variables.c
+++ C Programming Full Course for free/variables.c
@@ -5,5 +5,13 @@ int main(void)
     // variable = A reusable container for a value.
     //            Behaves as if it were the value it contains
 
+    int age = 25;
+    int year = 2026;
+    int quantety = 1;
+
+    printf("You are %d years old\n", age); // %i works too (but its not a decimal)
+    printf("The year is %d\n", year);
+    printf("You have ordered %d x items\n", quantety);
+
     return 0;
 }

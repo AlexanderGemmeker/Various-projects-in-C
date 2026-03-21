7d60ec1d78e9f522831811177e443443d819b71a
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-22 00:49:11 +0100

    a

--- C Programming Full Course for free/variables.c
+++ C Programming Full Course for free/variables.c
@@ -6,11 +6,13 @@ int main(void)
     //            Behaves as if it were the value it contains
 
     double pi = 3.14159265358979;
+    double e = 2.7182818284590;
 
-    printf("The value of pi is %lf\n", pi); // % Format Specifiers long float
+    printf("The value of pi is %.15lf\n", pi); // % Format Specifiers long float
+    printf("The value of e is %.15lf\n", e);
 
 /*
-__________________________________________________________________________________
+//________________________________________________________________________________
     float gpa = 2.5; // great point average
     float price = 19.99;
     float temperature = -10.1;
@@ -18,7 +20,7 @@ ________________________________________________________________________________
     printf("Your gpa is %f\n", gpa);
     printf("Your price is $%f\n", price);
     printf("The temperture is %f\n", temperature);
-__________________________________________________________________________________
+//________________________________________________________________________________
 
     int age = 25;
     int year = 2026;
@@ -27,7 +29,7 @@ ________________________________________________________________________________
     printf("You are %d years old\n", age); // %i works too (but its not a decimal)
     printf("The year is %d\n", year); // % is a Format Specifiers in C
     printf("You have ordered %d x items\n", quantety);
-__________________________________________________________________________________
+//________________________________________________________________________________
 */
 
     return 0;

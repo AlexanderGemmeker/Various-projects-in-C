21a978fc52d53def493617664ad1df8d5458b41f
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-22 18:13:25 +0100

    a

--- C Programming Full Course for free/variables.c
+++ C Programming Full Course for free/variables.c
@@ -84,3 +84,12 @@ int main(void)
 */
     return 0;
 }
+
+// variable = A reusable container for  value.
+//            Behaves as if it were the value it contains.
+// int = whole numbers (4 bytes in modern systems)
+// float = single-precision decimal number (4 bytes)
+// double = doueble-precision decimal number (8 bytes)
+// char = single character (1 byte)
+// char[] = array of characters (size varies)
+// bool = true or false (1 byte, requires <stdbool.h>)

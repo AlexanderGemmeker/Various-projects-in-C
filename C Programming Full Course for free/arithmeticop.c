3f2e016209d9bb0a6ddef2b62e42e30aae4c3030
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-22 22:36:59 +0100

    a

--- C Programming Full Course for free/arithmeticop.c
+++ C Programming Full Course for free/arithmeticop.c
@@ -3,14 +3,14 @@
 int main(void)
 {
     // arithmetic operators = + - * / % ++ --
-    int x = 2;
-    int y = 3;
+    float x = 2;
+    float y = 3;
     float z = 0;
 
     // z = x + y;
     // z = x - y;
     // z = x * y;
-    // z = x / y;
+    z = x / y;
 
     printf("%f\n", z);
 

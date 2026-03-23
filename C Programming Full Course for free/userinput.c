3f2b786bf62a81964c7f5bff4681d5a395da4f9b
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-23 21:31:35 +0100

    a

---
+++ C Programming Full Course for free/userinput.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    int age = 0; // Everything to 0
+    float gpa = 0.0f; // 0.0f so everybody can see its a floatingpoint
+    char grade = '\0'; // \0 Null terminator
+    char name[30] = ""; // 30 char. long, its the maximum size then
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("%d\n", age);
+    printf("%f\n", gpa);
+    printf("%c\n", grade);
+    printf("%s\n", name);
+
+    return 0;
+
+/*
+    int age = 0;
+    float gpa = 0.0f; // 0.0f so everybody can see its a floatingpoint
+    char grade = '\0'; // \0 Null terminator
+    char name[30] = ""; // 30 char. long, its the maximum size then
+
+    printf("%d\n", age);
+    printf("%f\n", gpa);
+    printf("%c\n", grade);
+    printf("%s\n", name);
+*/
+}

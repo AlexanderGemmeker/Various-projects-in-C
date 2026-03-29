855d379b970ae0483dcf433f91b93c2eb44700fc
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-29 17:56:40 +0200

    a

---
+++ C Programming Full Course for free/temperatureconversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Temperatur Conversion Program
+
+int main(void){
+
+    char choice = '\0';
+    float fahrenheit = 0.0f;
+    float celsius = 0.0f;
+
+    
+
+    return 0;
+}

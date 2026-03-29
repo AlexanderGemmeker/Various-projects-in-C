be53c93f9401d0abe0b4e7c19f021346d470bded
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-29 14:33:51 +0200

    a

---
+++ C Programming Full Course for free/compoundinterestcalc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+// Compound Interest Calculator
+
+int main(void)
+{
+    double principal = 0.0;
+    double rate = 0.0;
+    int years = 0;
+    int timesCompunded = 0;
+    double toal = 0.0; // Dont use "return", because its a keyword in C
+
+    printf("Compount Interest Calculator\n");
+
+    printf("Enter the principal (P): ");
+    scanf("%lf", &principal);
+
+    printf("Enter the interest rate % (r): ");
+    scanf("%lf", &rate);
+    rate = rate / 100;
+
+    printf("Enter the # of years (t): ");
+    scanf("%d", &years);
+
+    printf("Enter # of times compounded per year (n) ");
+    scanf("%d", &timesCompunded);
+
+    return 0;
+}

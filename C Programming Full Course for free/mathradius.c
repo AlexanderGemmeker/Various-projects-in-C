a03ed51522f45a315ce3e2034ca0c24fcb173869
AlexanderGemmeker <agemmeker@gmail.com>
2026-03-29 14:08:13 +0200

    a

--- C Programming Full Course for free/mathradius.c
+++ C Programming Full Course for free/mathradius.c
@@ -5,14 +5,20 @@ int main(void)
 {
     double radius = 0.0;
     double area = 0.0;
+    double surfaceArea = 0.0; // Calculating a surface are of a sphere
+    double volume = 0.0;
     const double PI = 3.14159; // Best practise const all uppercase 
 
     printf("Enter the radius: ");
     scanf("%lf", &radius);
 
     area = PI * pow(radius, 2);
+    surfaceArea = 4 * PI * pow(radius, 2);
+    volume = (4.0 / 3.0) * PI * pow(radius, 3); // Prevent integer devision
 
-    printf("Area: %.2lf", area); // 2 digits after the decimal
+    printf("Area: %.2lf\n", area); // 2 digits after the decimal
+    printf("Surface Area: %.2lf\n", surfaceArea);
+    printf("Volume: %.2lf\n", volume);
 
     return 0;
 }

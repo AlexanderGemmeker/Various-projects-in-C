8c29dd4e0098e8eca5e6500916558cbb75b78adc
AlexanderGemmeker <agemmeker@gmail.com>
2026-04-01 21:10:38 +0200

    a

--- C Programming Full Course for free/movietickets.c
+++ C Programming Full Course for free/movietickets.c
@@ -5,15 +5,32 @@ int main(void){
     
     float price = 10.00;
     bool isStudent = true; // 10% discount
-    bool isSenior = false; // 20% discount
+    bool isSenior = true; // 20% discount
 
+    // student = $9
+    // senior = $8
+    // student + senior = $7
+
+    // Nested IF statements
     if(isStudent){
+        if(isSenior){
+        printf("You get a student discount of 10%\n");
+        printf("You get a senior discount of 20%\n");
+        price *= 0.7;
+    }       
+    else{
         printf("You get a student discount of 10%\n");
         price *= 0.9;
     }
-    else if(isSenior){
-        
     }
+    else{
+        if(isSenior){
+            printf("You get a senior discount of 20%\n");
+            price *= 0.8;
+        }
+    }
+
+    
 
     printf("The price of a ticket is: $%.2f\n", price);
 

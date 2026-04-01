335ad5cbcf6c4422651727df994be33d31e20877
AlexanderGemmeker <agemmeker@gmail.com>
2026-04-01 20:58:13 +0200

    a

--- C Programming Full Course for free/movietickets.c
+++ C Programming Full Course for free/movietickets.c
@@ -8,8 +8,14 @@ int main(void){
     bool isSenior = false; // 20% discount
 
     if(isStudent){
+        printf("You get a student discount of 10%\n");
+        price *= 0.9;
+    }
+    else if(isSenior){
         
     }
+
+    printf("The price of a ticket is: $%.2f\n", price);
 
     // student = $9
     // senior = $8

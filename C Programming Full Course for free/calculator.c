c7e664f5e4b8676810a6d222c05ec5b250b44282
AlexanderGemmeker <agemmeker@gmail.com>
2026-04-07 22:48:47 +0200

    a

--- C Programming Full Course for free/calculator.c
+++ C Programming Full Course for free/calculator.c
@@ -5,6 +5,15 @@ int main(void){
     // Calculator Program
 
     char operator = '\0';
+    double num1 = 0.0;
+    double num2 = 0.0;
+    double result = 0.0;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the operator (+ - * /): ");
+    scanf("%c", &num1);
 
     return 0;
 }

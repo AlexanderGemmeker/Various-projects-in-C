344f0134bad231cc7c3e90754771f5156b9d6aaf
AlexanderGemmeker <agemmeker@gmail.com>
2026-04-14 22:33:19 +0200

    a

--- C Programming Full Course for free/functions.c
+++ C Programming Full Course for free/functions.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-int main(void){
+void happyBirthday(char birthdayboi[], int yearsOld){
+    printf("\nHappy birthday to you! ");
+    printf("\nHappy birthday to you! ");
+    printf("\nHappy birthday dear %s!", birthdayboi);
+    printf("\nHappy birthday to you! ");
+    printf("\nYou are %d years old!\n", yearsOld);
+}
+
+int main() {
 
     // function = A reusable section of code that can be invoked "called"
     //            Arguments can be sent to a function so that it can use them
 
-    return = 0;
+    char name[] = "Bro";
+    int age = 25;
+
+    happyBirthday(name, age);
+
+    return 0;
 }
